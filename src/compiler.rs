8704fa9f06b6c9a74d1767a73e3fba059f7b42fd
rrokkam <rohithrokkam@gmail.com>
2020-09-13 23:58:30 -0700

    Begin implementing Parser as an iter over Ops

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,6 +1,14 @@
 use crate::chunk::Chunk;
+use crate::chunk::Op;
 use crate::lexer::{Token, Tokens};
 use crate::InterpretError;
+use std::iter::Peekable;
+
+struct Parser<'source> {
+    tokens: Peekable<Tokens<'source>>,
+    had_error: bool,
+    panicking: bool,
+}
 
 // Pratt's parsing algorithm: https://craftinginterpreters.com/compiling-expressions.html
 pub fn compile(source: &str) -> Result<Chunk, InterpretError> {
@@ -12,3 +20,21 @@ pub fn compile(source: &str) -> Result<Chunk, InterpretError> {
 }
 
 fn expression(token: Token) {}
+
+impl Parser<'_> {
+    fn error(&mut self, token: &Token, message: &str) {
+        self.had_error = true;
+        if self.panicking {
+            return;
+        }
+        eprintln!("Got an error at {:?}", token);
+        eprintln!("{}", message);
+    }
+}
+
+impl Iterator for Parser<'_> {
+    type Item = Op;
+    fn next(&mut self) -> Option<Op> {
+        Some(Op::Return)
+    }
+}
